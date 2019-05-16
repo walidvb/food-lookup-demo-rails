40c0cb4c96755e8bf220b66a9d243756a9006331
Walid <hello@walidvb.com>
2019-05-16 18:33:19 -0300

    intercept requests and cache results. Return custom response if network failed or cache failed

--- client/public/sw.js
+++ client/public/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event){
   console.log('WORKER: install event in progress.');
   event.waitUntil(
     caches
-      .open(version + 'gri')
+      .open(version + 'static')
       .then(function(cache){
         return cache.addAll([
           '/',
@@ -22,13 +22,40 @@ self.addEventListener('fetch', function(event){
     return
   }
 
-  event.respondWith(new Promise(function(resolve, reject){
-    var response = new Response('yessss', {
-      status: 200,
-      headers: new Headers({
-        'Content-Type': 'text/html',
-      })
+  event.respondWith(
+    caches
+    .match(event.request)
+    .then(function(cached){
+      var networked = fetch(event.request)
+        .then(fetchedFromNetwork, unableToResolve)
+        .catch(unableToResolve);
+
+        return cached || networked;
+        function fetchedFromNetwork(response){
+          var cacheCopy = response.clone();
+          console.log('WORKER: fetch response', event.request.url);
+
+          caches
+            .open(version+'pages')
+            .then(function add(cache){
+              cache.put(event.request, cacheCopy)
+            })
+            .then(function(){
+              console.log('WORKER: fetch stored in cache', event.request.url);
+            })
+        }
+
+        function unableToResolve(){
+          var emptyRes = JSON.stringify([{ description: "No result", kcal: 717, fat_g: 75.43, carbohydrate_g: 0.06, protein_g: 0.85 }])
+          return new Response(emptyRes, {
+            status: 200,
+            headers: {
+              'Content-Type': 'application/json',
+            }
+          })
+        }
     })
-    resolve(response)
-  }))
+
+
+  )
 })
