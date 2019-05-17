ab3e744563ae35d23fb650f03da1a873401b1604
Walid <hello@walidvb.com>
2019-05-17 12:25:28 -0300

    fixup: display sent items

--- client/src/FoodForm.js
+++ client/src/FoodForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 
 export default class FoodForm extends React.Component {
-  state = { food: { description: '' } }
+  state = { food: { description: '' }, sent: [] }
   handleOnChange = ({ target: { value: description } }) => this.setState({ food: { description }})
   handleSubmit = () => {
     const url = '/api/food'
@@ -11,7 +11,8 @@ export default class FoodForm extends React.Component {
     axios.post(url, { food })
       .then(() => {
         this.setState({
-          food: { description: ''}
+          food: { description: ''},
+          sent: [food, ...this.state.sent],
         })
       });
   }
@@ -20,6 +21,7 @@ export default class FoodForm extends React.Component {
       <div>
         <input type="text" onChange={this.handleOnChange} value={this.state.description}></input>
         <input type="button" value="Submit" onClick={this.handleSubmit}/>
+        {this.state.sent.map((f,i) => <div key={i}><em>sent:</em> {f.description}</div>)}
       </div>
     )
   }
